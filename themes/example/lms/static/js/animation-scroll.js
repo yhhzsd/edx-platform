58ef3387e79af7e4110199b2f1343e9b9050d97a
hasnain naveed <hasnain.naveed123@gmail.com>
2016-07-19 10:37:30 +0000

    small changes

--- themes/example/lms/static/js/animation-scroll.js
+++ themes/example/lms/static/js/animation-scroll.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+    "use strict";
     $('.back-to-top').on('click', function(event) {
         event.preventDefault();
         $('html, body').animate({scrollTop: 0}, 300);

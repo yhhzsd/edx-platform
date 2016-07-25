bf0bab3ceb829df57addd9fd460042111a41c4b1
Eric Fischer <efischer@edx.org>
2016-07-25 15:46:58 -0400

    revert me pls

--- lms/envs/devstack.py
+++ lms/envs/devstack.py
@@ -280,3 +280,5 @@ if os.path.isfile(join(dirname(abspath(__file__)), 'private.py')):
 MODULESTORE = convert_module_store_setting_if_needed(MODULESTORE)
 
 SECRET_KEY = '85920908f28904ed733fe576320db18cabd7b6cd'
+
+FEATURES['GITHUB_ALLOW_EMPTY_PR'] = True
